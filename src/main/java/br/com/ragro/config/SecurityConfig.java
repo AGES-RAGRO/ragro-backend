a4e40af88c5010d644bdd2adbca7a46775b6dda1
Eduardo Rosa <eduardorosa10000@gmail.com>
2026-03-30 20:37:13 -0300

    fix: wrong name implementation

--- src/main/java/br/com/ragro/config/SecurityConfig.java
+++ src/main/java/br/com/ragro/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/actuator/health").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/farmer/**").hasRole("FARMER")
-                        .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                        .requestMatchers("/consumer/**").hasRole("CONSUMER")
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
