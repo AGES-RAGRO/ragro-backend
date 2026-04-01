67405fa42f325bd151b7873311fb121fc9328ad5
ArthurM-Maciel <arthur.maciel002@edu.pucrs.br>
2026-04-01 11:03:22 -0300

    feat: add relationships User -> Customer -> Address (#27)

--- src/main/java/br/com/ragro/domain/User.java
+++ src/main/java/br/com/ragro/domain/User.java
@@ -10,6 +10,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -51,4 +53,10 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private OffsetDateTime updatedAt;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Customer customerProfile;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Address> addresses = new ArrayList<>();
 }
