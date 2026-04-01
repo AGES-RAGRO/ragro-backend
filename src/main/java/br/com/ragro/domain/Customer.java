513701d59610822a7855cfcad37e8edc44a219ac
ArthurM-Maciel <arthur.maciel002@edu.pucrs.br>
2026-04-01 00:19:16 -0300

    feat: create Customer entity and CustomerRepository (#23)

---
+++ src/main/java/br/com/ragro/domain/Customer.java
@@ -0,0 +1,41 @@
+package br.com.ragro.domain;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString(of = "id")
+public class Customer {
+
+    @Id
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(name = "fiscal_number", nullable = false, unique = true, length = 11)
+    private String fiscalNumber;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+}
