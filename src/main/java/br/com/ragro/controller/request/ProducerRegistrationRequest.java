5d7c16a6fc3f767201bff12ecbf4dfa01b43da91
JuliaTietbohl <julia.tietbohl002@edu.pucrs.br>
2026-04-10 19:49:41 -0300

    remove import notNull

--- src/main/java/br/com/ragro/controller/request/ProducerRegistrationRequest.java
+++ src/main/java/br/com/ragro/controller/request/ProducerRegistrationRequest.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
