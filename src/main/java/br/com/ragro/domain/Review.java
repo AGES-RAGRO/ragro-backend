7b8e3b2140a3c2bc39f3f3e0834a5e5cd43d0d8c
ArthurM-Maciel <arthur.maciel002@edu.pucrs.br>
2026-05-01 20:27:46 -0300

    feat(#286): add Review JPA entity

---
+++ src/main/java/br/com/ragro/domain/Review.java
@@ -0,0 +1,45 @@
+package br.com.ragro.domain;
+
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "review")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString(of = "id")
+
+
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id",columnDefinition = "uuid")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Producer farmer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn (name = "customer_id", nullable + false)
+    private Customer customer;
+
+    @Column(name = "rating", nullable = false)
+    private Short rating;
+
+    @Column(name = "comment", columnDefinition = "text")
+    private String comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at"),nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+}
