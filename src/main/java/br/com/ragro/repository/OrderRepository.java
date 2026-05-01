b086110a3d6fdc04515cf2951fb33696660fbd4b
ArthurM-Maciel <arthur.maciel002@edu.pucrs.br>
2026-05-01 20:33:20 -0300

    feat(#286): add findByIdAndCustomerId to OrderRepository

--- src/main/java/br/com/ragro/repository/OrderRepository.java
+++ src/main/java/br/com/ragro/repository/OrderRepository.java
@@ -2,7 +2,8 @@ package br.com.ragro.repository;
 
 import br.com.ragro.domain.Order;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    Optional<Order> findByIdAndCustomerId(UUID id, UUID customerId);
 }
