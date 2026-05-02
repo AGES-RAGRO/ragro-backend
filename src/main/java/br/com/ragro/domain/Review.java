c7b50aea53ed4fc571a5f995d369457204a58094
ArthurM-Maciel <arthur.maciel002@edu.pucrs.br>
2026-05-01 21:05:56 -0300

    fix(#286): use Integer for rating and add null check in ReviewMapper

--- src/main/java/br/com/ragro/domain/Review.java
+++ src/main/java/br/com/ragro/domain/Review.java
@@ -2,14 +2,12 @@ package br.com.ragro.domain;
 
 import jakarta.persistence.*;
 import java.time.OffsetDateTime;
-import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "review")
@@ -17,29 +15,32 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Setter
 @EqualsAndHashCode(of = "id")
 @ToString(of = "id")
-
-
 public class Review {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id",columnDefinition = "uuid")
+    @Column(name = "id", columnDefinition = "uuid")
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "order_id", nullable = false)
-    private Producer farmer;
+    private Order order;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn (name = "customer_id", nullable + false)
+    @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "farmer_id", nullable = false)
+    private Producer farmer;
+
     @Column(name = "rating", nullable = false)
-    private Short rating;
+    private Integer rating;
 
     @Column(name = "comment", columnDefinition = "text")
     private String comment;
 
     @CreationTimestamp
-    @Column(name = "created_at"),nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private OffsetDateTime createdAt;
 }
