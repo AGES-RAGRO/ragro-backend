a26fe6c484d7ab24c75c19c6c7adf813c06a27de
Diogo Pessin <diogpcam@gmail.com>
2026-05-10 16:54:31 -0300

    fix: removing unused import

--- src/test/java/br/com/ragro/service/ReviewServiceTest.java
+++ src/test/java/br/com/ragro/service/ReviewServiceTest.java
@@ -3,7 +3,6 @@ package br.com.ragro.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
