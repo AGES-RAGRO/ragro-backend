9486dbbbf323a045b02bf97773a127b393ddc024
JuliaTietbohl <julia.tietbohl002@edu.pucrs.br>
2026-05-18 22:10:41 -0300

    get calculo co2

--- src/main/java/br/com/ragro/repository/Co2SavingRepository.java
+++ src/main/java/br/com/ragro/repository/Co2SavingRepository.java
@@ -3,8 +3,13 @@ package br.com.ragro.repository;
 import br.com.ragro.domain.Co2Saving;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Co2SavingRepository extends JpaRepository<Co2Saving, UUID> {
+
+    @Query("SELECT COALESCE(SUM(c.co2Saved), 0) FROM Co2Saving c")
+Double sumAllCo2Saved();
 }
+
